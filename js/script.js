de705311066874df02c7728b0cbb516d5104a9a7
anatolethien <anatole.thien@ynov.com>
2021-04-10 14:46:38 +0200

    14

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
-const list = ["Chips", "Ham", "Lemonade"];
+// Import list from HTML document
+let item_list = ["Bread", "Chips", "Ham", "Lemonade", "Sweets"];
 
-function add_new_article() {
-    let li = document.createElement("li");
-    let new_article = document.getElementById("new_article").value;
-    let text = document.createTextNode(new_article);
-    li.appendChild(text);
-    if (new_article === "") {
-        alert("Error: empty string.");
-    } else {
-        document.getElementById("article_list").appendChild(li);
-    }
-    document.getElementById("new_article").value = "";
+function get_item_list() {
+
 }
 
-console.log(list);
+function add_new_item() {
+    let new_item = document.getElementById("new_item").value;
+    if (new_item !== ""){
+        item_list.push(new_item);
+        console.log("Adding new_item = \"" + new_item + "\" to: item_list");
+    } else {
+        alert("Error: empty string.");
+        console.log("Error: empty string.");
+    }
+    display_item_list()
+}
+
+function display_item_list() {
+    let element = document.getElementById("item_list");
+    let data = "";
+    if (item_list.length > 0) {
+        for (current_item in item_list) {
+            data += "<li><span>" + item_list[current_item] + "</span><button type=\"submit\"><i class=\"gg-math-minus\"></i></button></li>"
+        }
+    }
+    return element.innerHTML = data;
+}
+
+display_item_list();
