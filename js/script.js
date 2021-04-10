aabe894f131ccf48894bb793f97edac29fc397d3
anatolethien <anatole.thien@ynov.com>
2021-04-10 17:00:54 +0200

    15

--- js/script.js
+++ js/script.js
@@ -5,16 +5,23 @@ function get_item_list() {
 
 }
 
-function add_new_item() {
-    let new_item = document.getElementById("new_item").value;
-    if (new_item !== ""){
-        item_list.push(new_item);
-        console.log("Adding new_item = \"" + new_item + "\" to: item_list");
+function add_item() {
+    let new_item = document.getElementById("new_item");
+    if (new_item.value !== "") {
+        item_list.push(new_item.value);
+        console.log("Adding new_item = \"" + new_item.value + "\" to item_list");
+        new_item.value = "";
     } else {
         alert("Error: empty string.");
         console.log("Error: empty string.");
     }
-    display_item_list()
+    display_item_list();
+}
+
+function clean_item_list() {
+    item_list = [];
+    console.log("Cleaning item_list");
+    display_item_list();
 }
 
 function display_item_list() {
@@ -22,8 +29,10 @@ function display_item_list() {
     let data = "";
     if (item_list.length > 0) {
         for (current_item in item_list) {
-            data += "<li><span>" + item_list[current_item] + "</span><button type=\"submit\"><i class=\"gg-math-minus\"></i></button></li>"
+            data += "<li><span>" + item_list[current_item] + "</span><button type=\"submit\" id=\"remove_item\"><i class=\"gg-math-minus\"></i></button></li>";
         }
+    } else {
+        data = "<li><span>No article yet!</span><button type=\"submit\" id=\"remove_item\"><i class=\"gg-math-minus\"></i></button></li>";
     }
     return element.innerHTML = data;
 }
