9d8501bea41c43012473ac9f4f8fb4afb44b5ef1
mail@anatolethien.eu <mail@anatolethien.eu>
2022-02-26 12:39:24 +0100

    revamp

--- js/script.js
+++ js/script.js
@@ -1,48 +1,63 @@
-let item_list = [];
-
-function add_item() {
-    let item = document.getElementById("new_item");
-    if (item.value !== "") {
-        item_list.push(item.value);
-        console.log("Adding \"" + item.value + "\" at item_list[" + (item_list.length - 1) + "]");
-        item.value = "";
-    } else {
-        alert("Error: empty string.");
-        console.log("Error: empty string.");
-    }
-    display_item_list();
+class Groceries {
+	constructor() {
+		this.list = [];
+	}
+	add(value) {
+		if (value !== '') {
+			this.list.push(value);
+		} else {
+			alert('Error: empty string.');
+		}
+		article.value = '';
+		display();
+	}
+	remove(index) {
+		this.list.splice(index, 1);
+		display();
+	}
+	clear() {
+		this.list = [];
+		display();
+	}
 }
 
-function remove_item(item) {
-    console.log("Removing \"" + item_list[item] + "\" from item_list[" + item + "]");
-    item_list.splice(item, 1);
-    display_item_list();
+const groceries = new Groceries();
+
+const form = document.getElementById('form');
+const article = document.getElementById('article');
+const add = document.getElementById('add');
+const list = document.getElementById('list');
+const data = document.getElementById('data');
+const clear = document.getElementById('clear');
+
+function display() {
+	list.textContent = '';
+	if (groceries.list.length > 0) {
+		for (var item in groceries.list) {
+			let li = document.createElement('li');
+			let span = document.createElement('span');
+			span.textContent = groceries.list[item];
+			let button = document.createElement('button');
+			button.classList.add('remove');
+			button.onclick = function () {
+				groceries.remove(item);
+			}
+			let img = document.createElement('img');
+			img.src = 'img/minus.svg';
+			img.alt = 'Remove';
+			button.appendChild(img);
+			li.appendChild(span);
+			li.appendChild(button);
+			list.appendChild(li);
+		}
+	} else {
+		let li = document.createElement('li');
+		let span = document.createElement('span');
+		span.textContent = 'No article yet!';
+		li.appendChild(span);
+		list.appendChild(li);
+	}
+	data.innerHTML = `You have ${groceries.list.length} article(s).`;
 }
 
-function clean_item_list() {
-    item_list = [];
-    console.log("Cleaning item_list");
-    display_item_list();
-}
-
-function item_number() {
-    let item_number = item_list.length;
-    document.getElementById("item_number").innerHTML = String(item_number);
-}
-
-function display_item_list() {
-    item_number();
-    let element = document.getElementById("item_list");
-    let li = "";
-    if (item_list.length > 0) {
-        for (item in item_list) {
-            li += "<li><span>" + item_list[item] + "</span><button type=\"submit\" onclick=\"remove_item(" + item + ")\"><i class=\"gg-math-minus\"></i></button></li>";
-        }
-    } else {
-        li = "<li><span>No article yet!</span></li>";
-    }
-    console.log("Displaying item_list = " + item_list);
-    return element.innerHTML = li;
-}
-
-display_item_list();
+display();
